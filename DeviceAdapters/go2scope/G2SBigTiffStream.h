2008ccbfd0626dbaa337ebaf3e5d0a3f3cd94e48
Miloš Jovanović <milos@tehnocad.rs>
2024-11-26 15:23:17 +0100

    G2S Storage driver: G2S format (BigTIFF) file stream class

---
+++ DeviceAdapters/go2scope/G2SBigTiffStream.h
@@ -0,0 +1,118 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+// FILE:          G2SBigTiffStream.h
+// PROJECT:       Micro-Manager
+// SUBSYSTEM:     DeviceAdapters
+//-----------------------------------------------------------------------------
+// DESCRIPTION:   Go2Scope devices. Includes the experimental StorageDevice
+//
+// AUTHOR:        Milos Jovanovic <milos@tehnocad.rs>
+//
+// COPYRIGHT:     Nenad Amodaj, Chan Zuckerberg Initiative, 2024
+//
+// LICENSE:       This file is distributed under the BSD license.
+//                License text is included with the source distribution.
+//
+//                This file is distributed in the hope that it will be useful,
+//                but WITHOUT ANY WARRANTY; without even the implied warranty
+//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+//
+// NOTE:          Storage Device development is supported in part by
+//                Chan Zuckerberg Initiative (CZI)
+// 
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+#include <string>
+#include <vector>
+#include "G2SFileUtil.h"
+
+/**
+ * BigTIFF file stream descriptor / state cache
+ * @author Miloš Jovanović <milos@tehnocad.rs>
+ * @version 1.0
+ */
+class G2SBigTiffStream
+{
+public:
+	//============================================================================================================================
+	// Constructors & Destructors
+	//============================================================================================================================
+	G2SBigTiffStream(const std::string& path, bool fbig, bool dio, std::uint32_t chunk = 0) noexcept;
+	G2SBigTiffStream(const G2SBigTiffStream& src) noexcept = default;
+	~G2SBigTiffStream() noexcept { close(); }
+
+public:
+	//============================================================================================================================
+	// Public interface
+	//============================================================================================================================
+	void															open(bool trunc);
+	void															close() noexcept;
+	void															parse(std::string& datasetuid, std::vector<std::uint32_t> shape, std::vector<unsigned char>& metadata, std::uint64_t& metaoffset, std::uint32_t& bitdepth);
+	void															formHeader() noexcept;
+	void															writeShapeInfo(const std::vector<std::uint32_t>& shape) noexcept;
+	void															setChunkIndex(std::uint32_t val) noexcept;
+	void															appendMetadata(const std::vector<unsigned char>& meta);
+	std::size_t													commit(const unsigned char* buff, std::size_t len);
+	std::size_t													write(const unsigned char* buff, std::size_t len);
+	std::size_t													fetch(unsigned char* buff, std::size_t len);
+	std::size_t													read(unsigned char* buff, std::size_t len);
+	std::uint64_t												seek(std::int64_t pos, bool beg = true);
+	std::uint64_t												offset(std::int64_t off);
+	void															flush() const;
+	std::uint64_t												getFileSize() const noexcept;
+	void															appendIFD(std::uint32_t imgw, std::uint32_t imgh, std::uint32_t imgdepth, std::size_t imagelen, const std::string& meta);
+	std::size_t													setIFDTag(unsigned char* ifd, std::uint16_t tag, std::uint16_t dtype, std::uint64_t val, std::uint64_t cnt = 1) const noexcept;
+	std::uint32_t												getTagCount(const std::string& meta) const noexcept { return meta.empty() ? G2STIFF_TAG_COUNT_NOMETA : G2STIFF_TAG_COUNT; }
+	std::uint64_t												parseIFD(std::vector<unsigned char>& ifd, std::uint32_t& ifdsz);
+	void															loadNextIFD() { loadIFD(currentifdpos); }
+	void															loadIFD(std::uint64_t off);
+	void															calcDescSize(std::size_t metalen, std::uint32_t tags, std::uint32_t* ifd, std::uint32_t* desc, std::uint32_t* tot) noexcept;
+	void															moveReadCursor(std::uint64_t pos) noexcept;
+	void															moveWriteCursor(std::uint64_t pos) noexcept;
+#ifdef _WIN32
+	bool															isOpen() const noexcept { return fhandle != nullptr; }
+#else
+	bool															isOpen() const noexcept { return fhandle > 0; }
+#endif
+
+private:
+	//============================================================================================================================
+	// Data members - Stream state / cache
+	//============================================================================================================================
+	std::vector<unsigned char>								header;											///< Header (cache)
+	std::vector<unsigned char>								lastifd;											///< Last IFD (cache)
+	std::vector<unsigned char>								currentifd;										///< Current IFD (cache)
+	std::vector<unsigned char>								writebuff;										///< Write buffer for direct I/O
+	std::vector<unsigned char>								readbuff;										///< Read buffer for direct I/O
+	std::size_t													readbuffoff;									///< Read buffer offset
+	std::uint64_t												currpos;											///< Current file stream offset
+	std::uint64_t												writepos;										///< Write stream offset
+	std::uint64_t												readpos;											///< Read stream offset
+	std::uint64_t												lastifdpos;										///< Offset of the last image descriptor
+	std::uint32_t												lastifdsize;									///< Last IFD size
+	std::uint64_t												currentifdpos;									///< Offset of the current image descriptor
+	std::uint32_t												currentifdsize;								///< Current IFD size
+	std::uint64_t												nextifdpos;										///< Offset of the next image descriptor
+	std::uint32_t												currentimage;									///< Current image index (used for reading only)
+	std::uint32_t												imgcounter;										///< Image counter
+	std::uint32_t												chunkindex;										///< Chunk index
+	std::vector<std::uint64_t>								ifdcache;										///< IFD offset cache
+#ifdef _WIN32
+	void*															fhandle;											///< File handle
+#else
+	int															fhandle;											///< File descriptor
+#endif
+
+private:
+	//============================================================================================================================
+	// Data members - Configuration
+	//============================================================================================================================
+	std::string													fpath;											///< File path
+	std::uint32_t												ssize;											///< Sector size (for direct I/O)
+	bool															directIo;										///< Use direct I/O for file operations
+	bool															bigTiff;											///< Use big TIFF format
+};
+
